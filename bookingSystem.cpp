26241c170f1b269bda138581ab2d65514db34db1
Rodrigo Rebelo <rrebelo553@gmail.com>
2022-04-06 17:02:18 -0300

    Menu added

--- bookingSystem.cpp
+++ bookingSystem.cpp
@@ -7,8 +7,7 @@ using namespace std;
 #define LIN 5
 #define COL 40
 
-
-
+int resposta;
 
 void mostra_matriz(int **mat)
 {
@@ -44,11 +43,77 @@ void mostra_matriz(int **mat)
 	}
 }
 
+void reserva_de_lugares(int **x)
+{
+
+	int resposta;
+	int selecionarpoltrona;
+	int selecionarfileira;
+	int selecionarcoluna;
+
+	for (int C=1; C < 15; C++)
+	{
+		for (int F=1; F < 40; F++)
+		{
+		cout << setw(1) << (x[C][F]==0?".":"#") << "| ";
+		}
+	}
+
+	cout <<"\n\n Qual fileira deseja reservar?: ";
+	cin >> selecionarfileira;
+	cout <<"\n\n Qual coluna deseja reservar?: ";
+	cin >> selecionarcoluna;
+
+	if(selecionarcoluna <=5)
+	{
+		if(x[selecionarcoluna][selecionarfileira] == 0)
+		{
+			x[selecionarcoluna][selecionarfileira] = 1;
+			cout << "assento reservado\n\n";
+		}
+	}
+
+	else
+	{
+		cout << "assento indisponivel ";
+	}
+
+}
+
+
+
 int main()
 {
-    int **teste;
 
-	mostra_matriz(teste);
-	cout << "=======================" << endl;
+		do
+	{
+	
+		int **teste;
+		int **r;
+		int resposta;
+
+
+	    cout<<"0. Finalizar\n1. Reservar Poltrona\n2. Mapa de ocupaçãp\n3. Faturamento\n";
+        cin >> resposta;
+
+		switch(resposta)
+			{
+				case 0:
+				return 0;
+				case 1:
+					cout << "=======================" << endl;
+				reserva_de_lugares(r);
+				break;
+				case 2:
+					cout << "=======================" << endl;
+				mostra_matriz(teste);
+				break;
+				case3:
+				cout << "Teste3";
+				break;
+			}
+
+	} while (resposta =! 0);
+
 
 }
