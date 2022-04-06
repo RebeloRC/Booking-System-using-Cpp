db04b82de91205fbe27e060f7cab0371f46565cf
Rodrigo Rebelo <rrebelo553@gmail.com>
2022-04-06 13:38:50 -0300

    Adding two-dimensional array

--- bookingSystem.cpp
+++ bookingSystem.cpp
@@ -1,126 +1,54 @@
 #include<string>
 #include<iostream>
+#include <iomanip>
 
 using namespace std;
 
-int main()
+#define LIN 5
+#define COL 40
 
+
+
+
+void mostra_matriz(int **mat)
 {
-    int qtpoltronas;
-    int npoltrona;
-    int F1[600];
-    int i;
-    int preco;
+	int i;
+	int j;
+	int k;
+	
+	// Inicialização de cada "linha" do aray
+	mat = new int*[5];
+	
+	for(i=0; i<LIN; ++i) // Percorre as linhas
+	{
+		mat[i] = new int[40]; // Inicialização de cada coluna
+	}
+	// Fim da declaração do array
+	
+	k = 0;
+	for(i=0; i<LIN; ++i)
+	{
+		for(j = 0; j<COL; ++j)
+		{
+			mat[i][j] = 0;
+		}
+	}
+	
+	for(i=0; i<LIN; ++i)
+	{	
+		for(j = 0; j<COL; ++j)
+		{
+			cout << setw(1) << (mat[i][j]==0?".":"#")  << " | ";
+		}
+		cout << endl;
+	}
+}
 
+int main()
+{
+    int **teste;
 
-    for ( i = 0; i < 100; i++)
-    {
+	mostra_matriz(teste);
+	cout << "=======================" << endl;
 
-        cout<<"0. Finalizar\n1. Reservar Poltrona\n2. Mapa de ocupaçãp\n3. Faturamento\n";
-        int choice;
-        cin >> choice;
-
-        system("clear");
-
-            for (i = 0; i < sizeof(F1)/4; i++)
-        {
-            F1[i] = i;
-        }
-    
-        switch (choice)
-        {
-        case 0:
-            cout << "Compra Finalizada!\n";
-            
-            break;
-
-        case 1:
-
-            cout << "Quantos ingressos pretende comprar ?";
-            cin >> qtpoltronas;            
-
-            do
-            {
-                cout << "Digite o número da Poltrona!";
-                cin>> npoltrona;
-
-                for  (i = 0; i < sizeof(F1)/4; i++)
-                {
-                    if (F1[i] == npoltrona)
-                    {   
-
-                        if (F1[i] == 0 || npoltrona < 0 || npoltrona > 600)
-                        {
-                            cout << "A poltrona selecionada não está disponivel";
-                            qtpoltronas++;
-                        }
-                       
-                        else if (npoltrona <= 200)
-                        {
-                            F1[i] = 0;
-                            cout << "Poltrona "; cout << npoltrona; cout <<" selecionada com sucesso!\n";
-                            preco +=50;
-                        }
-
-                        else if (npoltrona > 200 & npoltrona <= 400)
-                        {
-                            F1[i] = 0;
-                            cout << "Poltrona "; cout << npoltrona; cout <<" selecionada com sucesso!\n";
-                            preco +=30;
-                        }
-
-                        else if (npoltrona > 400 & npoltrona <= 600)
-                        {
-                            F1[i] = 0;
-                            cout << "Poltrona "; cout << npoltrona; cout <<" selecionada com sucesso!\n";
-                            preco +=10;
-                        }
-
-                        else 
-                        {
-                            cout << "A poltrona solicitada é invalida!\n";
-                        }
-
-                    }
-
-                }
-                qtpoltronas--;
-            } while (qtpoltronas > 0);
-
-            cout<<"\n";
-
-            cout << "Mapa atualizado!\n";
-
-            for (i = 0; i < sizeof(F1)/4; i++)
-            {
-                cout << F1[i]; cout << "\t";
-            }            
-
-            cout<<"\n";
-
-            cout << "O preço todal da sua compra foi de R$"; cout << preco;
-            
-            break;
-
-        case 2:
-            cout << "Este é o mapa de ocupação da sala\n";
-            
-            for (i = 0; i < sizeof(F1)/4; i++)
-            {
-                cout << F1[i]; cout << "\t";
-            }
-
-             cout<<"\n";
-
-            break;
-
-        case 3:
-            cout << "O preço de sua compra é :\n";
-            
-            break;
-
-        default:
-            break;
-        }
-    }
 }
